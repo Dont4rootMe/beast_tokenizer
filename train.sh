644bb7f8b8800c1beb061197519bc2e904aa1925
Fedorov Artem <artem_defender2016@mail.ru>
2025-11-16 23:10:32 +0300

    deleting debug hardcode

--- train.sh
+++ train.sh
@@ -4,24 +4,11 @@ PYTHONPATH="${PYTHONPATH}:$(pwd)" python train/train_beast.py \
     --vocab-size 1000 \
     --degree 0 \
     --device cpu \
-    --fit-beast-max-samples 100 \
-    --fit-bpe-max-samples 500 \
+    --fit-beast-max-samples 5000 \
+    --fit-bpe-max-samples 25000 \
     --bpe-vocab-size 2048 \
     --beast-checkpoint-dir beast_tokenizer_checkpoint \
     --bpe-checkpoint-dir beast_bpe_tokenizer_checkpoint \
     --eval-results-dir eval_results \
-    --max-eval-samples 100
+    --max-eval-samples 2500
 
-# python train/train_beast.py \
-#     --batch-size 32 \
-#     --num-basis 50 \
-#     --vocab-size 1000 \
-#     --degree 0 \
-#     --device cpu \
-#     --fit-beast-max-samples 5000 \
-#     --fit-bpe-max-samples 25000 \
-#     --bpe-vocab-size 2048 \
-#     --beast-checkpoint-dir beast_tokenizer_checkpoint \
-#     --bpe-checkpoint-dir beast_bpe_tokenizer_checkpoint \
-#     --eval-results-dir eval_results \
-#     --max-eval-samples 2500
